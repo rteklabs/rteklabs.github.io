456f13fd3367c224ca5973018e3ce99eb8e6c72c
rteklabs <rtek84@gmail.com>
2026-09-25 11:04:39 +0800

    Keep Routes feature disabled by default

--- property-map/api-config.js
+++ property-map/api-config.js
@@ -1,1 +1,2 @@
 window.PROPERTY_MAP_DATA_API = "https://script.google.com/macros/s/AKfycbzNldlowoHIJEC5gKC5Q6hnqdeJ8MYEUM3d3C0k4BgEAKHCDYK1AALGAn3HuyLMetbIdw/exec";
+window.PROPERTY_MAP_ROUTES_ENABLED = false;
